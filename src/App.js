ca6acd5b95bc05e1d06e146091bb1e3f872ca026
Mario Cardoso <cardosomarioar@gmail.com>
2017-10-27 09:22:18 +0100

    add navigation and list post from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list', // list, create
     contacts: []
   }
   componentDidMount() {
@@ -20,21 +20,30 @@ class App extends Component {
 
     ContactAPI.remove(contact)
   }
+  createContact(contact) {
+    ContactAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
   render() {
     return (
-      <div>
-        {this.state.screen === 'list' && (
+      <div className="app">
+        <Route exact path="/" render={() => (
           <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-          onNavigate={() => {
-            this.setState({ screen: 'create' })
-          }}
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact/>
-        )}
+        )}/>
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push("/")
+            }}
+          />
+        )}/>
       </div>
     )
   }
